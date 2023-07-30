0291eba18d7cf14cad1c08bf8c395d257eeab372
Aslanidi Alexandra <aslanidialexandra@MacBook-Pro-Aslanidi.local>
2023-07-30 16:08:47 +0400

    change text_example

--- Src/tests/test_example.py
+++ Src/tests/test_example.py
@@ -1,7 +1,8 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-#from selenium.webdriver.chrome.options import Options
-#from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
 import pytest
 #import allure
 
@@ -9,20 +10,20 @@ import pytest
 
 class TestExample:
     def test_example(self):
-
-
         options = ChromeOptions()
         options.binary_location = r'/Applications/Google/Chrome.app/Contents/MacOS/Google/Chrome 114'
         driver = Chrome(options=options)
         driver.get("https://skillbox.ru/")
         assert 'Skillbox – образовательная платформа с онлайн-курсами.' == driver.title
         
-        from selenium import webdriver
+      
 
 
-    #driver = webdriver.Chrome(ChromeDriverManager().install())
-    #driver.set_window_size(1024, 600)
-    #driver.maximize_window()
+    def test_example_1(self):
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+        driver.set_window_size(1024, 600)
+        driver.maximize_window()
+        pass
 
 
 
