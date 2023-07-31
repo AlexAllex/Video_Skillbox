e67acac3b72807548bddd67e177cbb064c7788d0
Aslanidi Alexandra <aslanidialexandra@MacBook-Pro-Aslanidi.local>
2023-07-31 20:45:18 +0400

    add fils

---
+++ Src/tests/test_for_github.py
@@ -0,0 +1,77 @@
+from  selenium import  webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import Keys
+import time
+from selenium.webdriver.support.ui import Select
+
+
+
+class TestGitHab1:
+    def test_keys_1(self, set_up_browser):
+        driver = set_up_browser
+        driver.get('https://github.com/microsoft/vscode/issues')
+        #time.sleep(10)
+        driver.find_element(By.CSS_SELECTOR, '.header-search-button.placeholder.input-button.form-control ').click()
+        el = driver.find_element(By.ID, 'query-builder-test')
+        el.send_keys('in:title')
+        el.click()
+        actions_chains = webdriver.ActionChains(driver)
+        actions_chains.key_down(Keys.SPACE).click()
+        el.send_keys('bug')
+        el.click()
+        
+
+
+    def test_keys_2(self, set_up_browser):
+        driver = set_up_browser
+        driver.get('https://github.com/microsoft/vscode/issues')
+        #time.sleep(10)
+        driver.find_element(By.CSS_SELECTOR, '.btn-link[title="Author"] ').click()
+        el1 = driver.find_element(By.ID, 'author-filter-field')
+        el1.send_keys('bpasero')
+        el1.click()
+        pass
+    
+    
+    def test_keys_3(self, set_up_browser):
+        driver = set_up_browser
+        driver.get('https://github.com/search/advanced')
+        #time.sleep(10)
+        driver.find_element(By.ID, "search_language").click()
+        select = Select(driver.find_element(By.TAG_NAME, "select"))
+        select.select_by_visible_text("Python")
+        el1 = driver.find_element(By.ID, 'search_stars')
+        el1.send_keys('>20000')
+        el2 = driver.find_element(By.ID, 'search_filename')
+        el2.send_keys('environment.yml')
+        driver.find_element(By.CSS_SELECTOR, ".form-group.flattened .btn.flex-auto").click()
+         
+        pass
+    
+    
+    #def test_keys_4(self, set_up_browser):
+        #driver = set_up_browser
+        #driver.get("https://skillbox.ru/")
+        #driver.find_element(By.CSS_SELECTOR, "[for='A']").click()
+        #el = driver.find_element(By.XPASS, '(//*[contains(@class, "p-slider-handle")])[4]')
+        #action_chains = webdriver.ActionChains(driver)
+        #action_chains\
+           # .click_and_hold(el)\
+           # .move_by_offset(xoffset=50, yoffset=0)\   
+           # .perform()
+       # action_chains.release().perform()
+
+    def test_keys_4(self, set_up_browser):
+        driver = set_up_browser
+        driver.get("https://github.com/microsoft/vscode/graphs/commit-activity")
+        action_chains = webdriver.ActionChains(driver)
+        time.sleep(13)
+        action_chains.move_to_element( driver.find_element(By.CSS_SELECTOR, '.bar.mini.active'))\
+            .perform()
+        pass
+        
+       
+        
+        
+        
+
