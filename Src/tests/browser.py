06e0bf1b27b5d4d82676a30b426577bd0af44165
Aslanidi Alexandra <aslanidialexandra@MacBook-Pro-Aslanidi.local>
2023-08-03 17:09:00 +0400

    change test fils

--- Src/tests/browser.py
+++ Src/tests/browser.py
@@ -2,6 +2,8 @@
 import  pytest
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver import Remote
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture()
@@ -10,7 +12,17 @@ def set_up_browser ():
 
     options = ChromeOptions()
     driver = Chrome(options=options)
+    options.page_load_strategy = "normal"
     driver.get("https://skillbox.ru/")
-    driver.quit()
+    #driver.implicitly_wait(30)
     yield driver
     driver.quit()
+
+#def set_up_brower()
+    #options = Options()
+    #options.page_load_strategy = "normal" #(здесь можно выставить none or eager)
+    #driver = Remote(desired_capabilities={"browerName": "chrome", "browserVersion": "Latest"},
+                    command_executor="http://127.0.0.0:4444/wd/hub")
+    #options = Options
+    #driver.quit()
+    #yield driver
--- Src/tests/test_for_github.py
+++ Src/tests/test_for_github.py
@@ -19,8 +19,11 @@ class TestGitHab1:
         actions_chains.key_down(Keys.SPACE).click()
         el.send_keys('bug')
         el.click()
-        
+       # expected_value = "Bug" or 'bug'
+        #bug_name = driver.find_element(By.CSS_SELECTOR, ".Text-sc-17v1xeu-0.kWPXhV.search-match")
 
+
+        #assert bug_name.get_attribute('class') == expected_value
 
     def test_keys_2(self, set_up_browser):
         driver = set_up_browser
@@ -30,6 +33,11 @@ class TestGitHab1:
         el1 = driver.find_element(By.ID, 'author-filter-field')
         el1.send_keys('bpasero')
         el1.click()
+        #author = driver.find_element(By.CSS_SELECTOR, '.opened-by')
+        #expected_value = " bpasero"
+
+        #assert author.get_attribute('class') == expected_value
+        #assert author.text == expected_value
         pass
     
     
@@ -49,19 +57,21 @@ class TestGitHab1:
         pass
     
     
-    #def test_keys_4(self, set_up_browser):
-        #driver = set_up_browser
-        #driver.get("https://skillbox.ru/")
-        #driver.find_element(By.CSS_SELECTOR, "[for='A']").click()
-        #el = driver.find_element(By.XPASS, '(//*[contains(@class, "p-slider-handle")])[4]')
-        #action_chains = webdriver.ActionChains(driver)
-        #action_chains\
-           # .click_and_hold(el)\
-           # .move_by_offset(xoffset=50, yoffset=0)\   
-           # .perform()
-       # action_chains.release().perform()
-
     def test_keys_4(self, set_up_browser):
+        driver = set_up_browser
+        driver.get("https://skillbox.ru/code/")
+        #time.sleep(15)
+        driver.execute_script("window.scrollBy(0, 100);")
+        driver.find_element(By.CSS_SELECTOR, ".ui-radio-field__input[value='profession']").click()
+        el = driver.find_element(By.XPASS, '(//*[contains(@class, "p-slider-handle")])[4]')
+        action_chains = webdriver.ActionChains(driver)
+        action_chains\
+            .click_and_hold(el)\
+            .move_by_offset(xoffset=50, yoffset=0)\
+            .perform()
+        action_chains.release().perform()
+
+    def test_keys_5(self, set_up_browser):
         driver = set_up_browser
         driver.get("https://github.com/microsoft/vscode/graphs/commit-activity")
         action_chains = webdriver.ActionChains(driver)
